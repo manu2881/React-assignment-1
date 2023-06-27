15e05010fc5eda5623f2bb2839ab022160c70497
manu2881 <65648759+manu2881@users.noreply.github.com>
2023-06-27 12:51:59 +0530

    Initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
